1c3eff506fda286cb3449af4458f92ce17276a77
liuzehao <1437070389@qq.com>
2021-06-07 19:16:01 +0800

    多人赛Service层(数据表发生更改)

---
+++ src/main/java/com/example/erlysflexq/service/MultiraceService.java
@@ -0,0 +1,52 @@
+package com.example.erlysflexq.service;
+
+
+import com.example.erlysflexq.dao.InsertMulti;
+import com.example.erlysflexq.dao.MultiraceProperties;
+import com.example.erlysflexq.pojo.Multirace;
+import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MultiraceService {
+    @Autowired
+    MultiraceProperties multiraceProperties;
+
+    @Autowired
+    InsertMulti insertMulti;
+
+    public int insertMulti(
+            String name,Long fir,
+            Long sec, Long thr, Long fou, Long fif,
+            Long six, int sc
+    ){
+        boolean b = insertMulti.insertMulti(name, fir, sec, thr, fou, fif, six, sc);
+        if(b){
+            return HttpStatus.SC_OK;
+        }else{
+            return HttpStatus.SC_BAD_GATEWAY;
+        }
+    }
+
+    public List<Multirace> findAll(){
+        return multiraceProperties.findAll();
+    }
+    public int update(Multirace multirace){
+        return multiraceProperties.update(multirace);
+    }
+
+    public Multirace findOne(int id){
+        return MultiraceProperties.findOne(id);
+    }
+
+    public int delete(int id){
+        return multiraceProperties.delete(id);
+    }
+
+    public int insert(Multirace multirace){
+        return MultiraceProperties.insert(multirace);
+    }
+}
