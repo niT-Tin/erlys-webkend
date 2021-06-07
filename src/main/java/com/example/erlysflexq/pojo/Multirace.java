820fc6f6bf8d6549f0391640a0b66b7d2a2aae26
liuzehao <1437070389@qq.com>
2021-06-07 19:18:45 +0800

    多人赛实体类(映射到数据表)

---
+++ src/main/java/com/example/erlysflexq/pojo/Multirace.java
@@ -0,0 +1,14 @@
+package com.example.erlysflexq.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Multirace {
+    private Long id;
+
+
+}
