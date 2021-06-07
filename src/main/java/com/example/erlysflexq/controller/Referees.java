c79fabc86a74720e1221b5df156a19e556090b36
liuzehao <1437070389@qq.com>
2021-06-07 19:06:07 +0800

    裁判插入成绩控制(By LIU)

---
+++ src/main/java/com/example/erlysflexq/controller/Referees.java
@@ -0,0 +1,48 @@
+package com.example.erlysflexq.controller;
+
+
+import com.example.erlysflexq.pojo.User;
+import com.example.erlysflexq.service.MultiraceService;
+import com.example.erlysflexq.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import org.apache.http.HttpStatus;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/api")
+public class Referees {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    MultiraceService multiraceService;
+
+    @PostMapping("/updateoneUser")
+    @ApiOperation("修改单个信息（数据表发生更改）")
+    @CrossOrigin
+    public int updateOneUser(User user){
+        int update= userService.update(user);
+        if (update == 1) {
+            return HttpStatus.SC_OK;
+        } else
+            return HttpStatus.SC_FORBIDDEN;
+    }
+
+    @RequiresRoles({"admin", "referee"})
+    @PostMapping("/insertm")
+    @ApiOperation("根据名字插入多人赛信息")
+    @CrossOrigin
+    public int insertMultiRace(String name,Long fir,
+                               Long sec, Long thr, Long fou, Long fif,
+                               Long six, int sc){
+        return multiraceService.insertMulti(name, fir, sec, thr, fou, fif, six, sc);
+    }
+
+}
+
