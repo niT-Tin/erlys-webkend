5c0dab4b0b7ecbea4ccdec0e1f6cac8808d183c8
liuzehao <1437070389@qq.com>
2021-06-07 19:23:24 +0800

    用户信息dao层(数据库关联)

---
+++ src/main/java/com/example/erlysflexq/dao/UserinfoProperties.java
@@ -0,0 +1,44 @@
+package com.example.erlysflexq.dao;
+
+
+import com.example.erlysflexq.pojo.Userinfo;
+import org.springframework.stereotype.Component;
+import org.teasoft.bee.getData.SuidRichData;
+import org.teasoft.bee.osql.SuidRich;
+import org.teasoft.honey.osql.core.BeeFactory;
+
+import java.util.List;
+
+@Component
+public class UserinfoProperties {
+    public List<Userinfo> findAll(){
+//        SuidRich suidRich = BeeFactory.getHoneyFactory().getSuidRich();
+        return SuidRichData.selectAll(new Userinfo());
+    }
+
+
+    public int update(Userinfo userinfo){
+        SuidRich suidRich = BeeFactory.getHoneyFactory().getSuidRich();
+//        Condition
+        return suidRich.update(userinfo);
+    }
+
+
+    public Userinfo findOne(int id){
+        SuidRich suidRich = BeeFactory.getHoneyFactory().getSuidRich();
+        return suidRich.selectById(new Userinfo(), id);
+//        SuidRich suidRich
+    }
+
+
+    public  int delete(int id){
+        SuidRich suidRich = BeeFactory.getHoneyFactory().getSuidRich();
+        return suidRich.deleteById(Userinfo.class, id);
+    }
+
+
+    public int insert(Userinfo userinfo){
+        SuidRich suidRich = BeeFactory.getHoneyFactory().getSuidRich();
+        return suidRich.insert(userinfo);
+    }
+}
