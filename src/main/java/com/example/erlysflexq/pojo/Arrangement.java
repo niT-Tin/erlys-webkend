a5a2f9830dace84c132c29f1ed77731fd4092218
liuzehao <1437070389@qq.com>
2021-06-07 19:08:34 +0800

    赛程实体类（对应数据表）

---
+++ src/main/java/com/example/erlysflexq/pojo/Arrangement.java
@@ -0,0 +1,13 @@
+package com.example.erlysflexq.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Arrangement {
+    private String time;
+    private String schedule;
+}
