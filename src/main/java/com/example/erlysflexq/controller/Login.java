296334b30437e71dbc511d0c16e8d98150787d22
liuzehao <1437070389@qq.com>
2021-06-08 12:35:03 +0800

    修改登录返回信息增加返回角色等信息

--- src/main/java/com/example/erlysflexq/controller/Login.java
+++ src/main/java/com/example/erlysflexq/controller/Login.java
@@ -1,14 +1,13 @@
 package com.example.erlysflexq.controller;
 
+import com.example.erlysflexq.pojo.Userinfo;
 import io.swagger.annotations.ApiOperation;
 import org.apache.http.HttpStatus;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.teasoft.bee.erlys.gets.GetMyWish;
 
 @RestController
 @RequestMapping("/api")
@@ -17,28 +16,30 @@ public class Login {
     @PostMapping("/welogin")
     @ApiOperation("手机端登录")
     @CrossOrigin
-    public int weLogin(String account, String password) {
+    public Userinfo weLogin(String account, String password) {
         Subject subject = SecurityUtils.getSubject();
+        Userinfo userinfo = GetMyWish.selectByAccount(new Userinfo(), account);
         UsernamePasswordToken token = new UsernamePasswordToken(account, password);
         try{
             subject.login(token);
-            return HttpStatus.SC_OK;
+            return userinfo;
         }catch(Exception e){
-            return HttpStatus.SC_FORBIDDEN;
+            return null;
         }
     }
 
     @PostMapping("/login")
     @ApiOperation("Web端登录")
     @CrossOrigin
-    public int webLogin(String account, String password) {
+    public Userinfo webLogin(String account, String password) {
+        Userinfo userinfo = GetMyWish.selectByAccount(new Userinfo(), account);
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken(account, password);
         try{
             subject.login(token);
-            return HttpStatus.SC_OK;
+            return userinfo;
         }catch(Exception e){
-            return HttpStatus.SC_FORBIDDEN;
+            return null;
         }
     }
 }
