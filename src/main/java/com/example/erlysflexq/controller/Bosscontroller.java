58b06e9d8f9a986ac1532d24923c3cb232ec484a
liuzehao <1437070389@qq.com>
2021-06-08 16:13:28 +0800

    统一返回封装类

--- src/main/java/com/example/erlysflexq/controller/Bosscontroller.java
+++ src/main/java/com/example/erlysflexq/controller/Bosscontroller.java
@@ -2,8 +2,8 @@ package com.example.erlysflexq.controller;
 
 
 import com.example.erlysflexq.pojo.Administrator;
+import com.example.erlysflexq.pojo.RqObject;
 import com.example.erlysflexq.pojo.Userinfo;
-import com.example.erlysflexq.pojo.User;
 import com.example.erlysflexq.pojo.Userinfo;
 import com.example.erlysflexq.service.AdministratorService;
 import com.example.erlysflexq.service.RefereeService;
@@ -20,127 +20,136 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class Bosscontroller {
-    @Autowired
-    RefereeService refereeService;
-
-    @Autowired
-    AdministratorService administratorService;
-
-    @GetMapping("/getallAdministrator")
-    @ApiOperation("获取所有信息")
-    @CrossOrigin
-    @RequiresAuthentication
-    public List<Administrator> selectAllAdministrator(){
-        return administratorService.findAll();
-    }
-
-    @PostMapping("/updateoneAdministrator")
-    @ApiOperation("修改单个信息")
-    @CrossOrigin
-    @RequiresAuthentication
-    public int updateOneAdministrator(Administrator administrator){
-        int update = administratorService.update(administrator);
-        if(update == 1){
-            return HttpStatus.SC_OK;
-        }else
-            return HttpStatus.SC_FORBIDDEN;
-    }
-
-
-
-    @GetMapping("/getoneAdministrator")
-    @ApiOperation("获取单个信息")
-    @CrossOrigin
-    @RequiresAuthentication
-    public Administrator findOneAdministrator(Integer id){
-        return administratorService.findOne(id);
-    }
-
-    @PostMapping("/deleteoneAdministrator")
-    @ApiOperation("根据ID删除单个信息")
-    @CrossOrigin
-    @RequiresAuthentication
-    public int deleteByIdAdministrator(Integer id){
-        int delete = administratorService.delete(id);
-        if(delete == 1){
-            return HttpStatus.SC_OK;
-        }else
-            return HttpStatus.SC_FORBIDDEN;
-    }
-
+//    @Autowired
+//    RefereeService refereeService;
+//
+//    @Autowired
+//    AdministratorService administratorService;
+//
+//
+//    final String role1 = "admin";
+//
+//    @GetMapping("/getallAdministrator")
+//    @ApiOperation("获取所有信息")
+//    @CrossOrigin
+//    @RequiresAuthentication
+//    public RqObject selectAllAdministrator(){
+//        RqObject r = new RqObject();
+//
+//        List<Administrator> all = administratorService.findAll();
+//
+//        return ;
+//    }
+//
+//    @PostMapping("/updateoneAdministrator")
+//    @ApiOperation("修改单个信息")
+//    @CrossOrigin
+//    @RequiresAuthentication
+//    public int updateOneAdministrator(Administrator administrator){
+//        int update = administratorService.update(administrator);
+//        if(update == 1){
+//            return HttpStatus.SC_OK;
+//        }else
+//            return HttpStatus.SC_FORBIDDEN;
+//    }
+//
+//
+//
+//    @GetMapping("/getoneAdministrator")
+//    @ApiOperation("获取单个信息")
+//    @CrossOrigin
+//    @RequiresAuthentication
+//    public Administrator findOneAdministrator(Integer id){
+//        return administratorService.findOne(id);
+//    }
+//
+//    @PostMapping("/deleteoneAdministrator")
+//    @ApiOperation("根据ID删除单个信息")
+//    @CrossOrigin
+//    @RequiresAuthentication
+//    public int deleteByIdAdministrator(Integer id){
+//        int delete = administratorService.delete(id);
+//        if(delete == 1){
+//            return HttpStatus.SC_OK;
+//        }else
+//            return HttpStatus.SC_FORBIDDEN;
+//    }
+//
     @RequiresRoles({"admin", "referee"})
     @GetMapping("/selectscorelist")
     @ApiOperation("获取成绩列表降序排列")
     @CrossOrigin
     @RequiresAuthentication
-    public List<Userinfo> selectScoreList(){
-        return GetMyWish.selectAllAndSortGrade(new Userinfo(), "racescore");
+    public RqObject selectScoreList(){
+        RqObject r = new RqObject();
+        r.setUserinfoList(GetMyWish.selectAllAndSortGrade(new Userinfo(), "racescore"));
+        return r;
     }
-
-    @PostMapping("/insertoneAdministrator")
-    @ApiOperation("插入单个信息")
-    @CrossOrigin
-    public int insertOneAdministrator(Administrator administrator){
-        int insert = administratorService.insert(administrator);
-        if(insert == 1){
-            return HttpStatus.SC_OK;
-        }else
-            return HttpStatus.SC_FORBIDDEN;
-    }
-
-    @RequiresRoles("admin")
-    @GetMapping("/getallReferee")
-    @ApiOperation("获取所有信息")
-    @CrossOrigin
-    @RequiresAuthentication
-    public List<Userinfo> selectAllReferee(){
-        return refereeService.findAll();
-    }
-
-    @RequiresRoles("admin")
-    @PostMapping("/updateoneReferee")
-    @ApiOperation("修改单个信息")
-    @CrossOrigin
-    @RequiresAuthentication
-    public int updateOneReferee(Userinfo referee){
-        int update = refereeService.update(referee);
-        if(update == 1){
-            return HttpStatus.SC_OK;
-        }else
-            return HttpStatus.SC_FORBIDDEN;
-    }
-
-    @RequiresRoles("admin")
-    @GetMapping("/getoneReferee")
-    @ApiOperation("获取单个信息")
-    @CrossOrigin
-    public Userinfo findOneReferee(Integer id){
-        return refereeService.findOne(id);
-    }
-
-    @RequiresRoles("admin")
-    @PostMapping("/deleteoneReferee")
-    @ApiOperation("根据ID删除单个信息")
-    @CrossOrigin
-    @RequiresAuthentication
-    public int deleteByIdReferee(Integer id){
-        int delete = refereeService.delete(id);
-        if(delete == 1){
-            return HttpStatus.SC_OK;
-        }else
-            return HttpStatus.SC_FORBIDDEN;
-    }
-
-    @RequiresRoles("admin")
-    @PostMapping("/insertoneReferee")
-    @ApiOperation("插入单个信息")
-    @CrossOrigin
-    @RequiresAuthentication
-    public int insertOneReferee(Userinfo referee) {
-        int insert = refereeService.insert(referee);
-        if (insert == 1) {
-            return HttpStatus.SC_OK;
-        } else
-            return HttpStatus.SC_FORBIDDEN;
-    }
+//
+//    @PostMapping("/insertoneAdministrator")
+//    @ApiOperation("插入单个信息")
+//    @CrossOrigin
+//    public int insertOneAdministrator(Administrator administrator){
+//        int insert = administratorService.insert(administrator);
+//        if(insert == 1){
+//            return HttpStatus.SC_OK;
+//        }else
+//            return HttpStatus.SC_FORBIDDEN;
+//    }
+//
+//    @RequiresRoles("admin")
+//    @GetMapping("/getallReferee")
+//    @ApiOperation("获取所有信息")
+//    @CrossOrigin
+//    @RequiresAuthentication
+//    public List<Userinfo> selectAllReferee(){
+//        return refereeService.findAll();
+//    }
+//
+//    @RequiresRoles("admin")
+//    @PostMapping("/updateoneReferee")
+//    @ApiOperation("修改单个信息")
+//    @CrossOrigin
+//    @RequiresAuthentication
+//    public int updateOneReferee(Userinfo referee){
+//        int update = refereeService.update(referee);
+//        if(update == 1){
+//            return HttpStatus.SC_OK;
+//        }else
+//            return HttpStatus.SC_FORBIDDEN;
+//    }
+//
+//    @RequiresRoles("admin")
+//    @GetMapping("/getoneReferee")
+//    @ApiOperation("获取单个信息")
+//    @CrossOrigin
+//    public Userinfo findOneReferee(Integer id){
+//        return refereeService.findOne(id);
+//    }
+//
+//    @RequiresRoles("admin")
+//    @PostMapping("/deleteoneReferee")
+//    @ApiOperation("根据ID删除单个信息")
+//    @CrossOrigin
+//    @RequiresAuthentication
+//    public int deleteByIdReferee(Integer id){
+//        int delete = refereeService.delete(id);
+//        if(delete == 1){
+//            return HttpStatus.SC_OK;
+//        }else
+//            return HttpStatus.SC_FORBIDDEN;
+//    }
+//
+//    @RequiresRoles("admin")
+//    @PostMapping("/insertoneReferee")
+//    @ApiOperation("插入单个信息")
+//    @CrossOrigin
+//    @RequiresAuthentication
+//    public int insertOneReferee(Userinfo referee) {
+//        int insert = refereeService.insert(referee);
+//        if (insert == 1) {
+//            return HttpStatus.SC_OK;
+//        } else
+//            return HttpStatus.SC_FORBIDDEN;
+//    }
 }
