a3c2ed30076a95b5953a40d1e32e10f3b1799d06
liuzehao <1437070389@qq.com>
2021-06-08 18:29:17 +0800

    jwt工具类

---
+++ src/main/java/com/example/erlysflexq/utils/JwtUtil.java
@@ -0,0 +1,46 @@
+package com.example.erlysflexq.utils;
+
+import cn.hutool.core.date.DateField;
+import cn.hutool.core.date.DateUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtUtil {
+
+    //密钥
+    @Value("${emos.jwt.secret}")
+    private String secret;
+    //过期时间
+    @Value("${emos.jwt.expire}")
+    private Integer expire;
+
+    public String generateToken(Integer userId){
+        Date date = DateUtil.offset(new Date(), DateField.DAY_OF_YEAR, expire);
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        JWTCreator.Builder builder = JWT.create();
+        return builder.withClaim("userId", userId).withExpiresAt(date).sign(algorithm);
+    }
+
+    public int getUserId(String token){
+        DecodedJWT jwt = JWT.decode(token);
+        return jwt.getClaim("userId").asInt();
+    }
+
+    public void verifyToken(String token){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        verifier.verify(token);
+    }
+
+
+}
