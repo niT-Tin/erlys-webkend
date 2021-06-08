ce511bdad594b30e4c25345e346eecc26bd6c0c7
liuzehao <1437070389@qq.com>
2021-06-08 14:48:02 +0800

    增加ThreadLocalToken操作

--- src/main/java/com/example/erlysflexq/controller/Login.java
+++ src/main/java/com/example/erlysflexq/controller/Login.java
@@ -1,17 +1,22 @@
 package com.example.erlysflexq.controller;
 
+import com.example.erlysflexq.config.shiro.ThreadLocalToken;
 import com.example.erlysflexq.pojo.Userinfo;
 import io.swagger.annotations.ApiOperation;
 import org.apache.http.HttpStatus;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.teasoft.bee.erlys.gets.GetMyWish;
 
 @RestController
 @RequestMapping("/api")
 public class Login {
+
+    @Autowired
+    ThreadLocalToken threadLocalToken;
 
     @PostMapping("/welogin")
     @ApiOperation("手机端登录")
@@ -20,6 +25,7 @@ public class Login {
         Subject subject = SecurityUtils.getSubject();
         Userinfo userinfo = GetMyWish.selectByAccount(new Userinfo(), account);
         UsernamePasswordToken token = new UsernamePasswordToken(account, password);
+        threadLocalToken.setToken(token.toString());
         try{
             subject.login(token);
             return userinfo;
@@ -35,6 +41,7 @@ public class Login {
         Userinfo userinfo = GetMyWish.selectByAccount(new Userinfo(), account);
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken(account, password);
+        threadLocalToken.setToken(token.toString());
         try{
             subject.login(token);
             return userinfo;
