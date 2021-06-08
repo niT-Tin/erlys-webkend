934300f9c9658f8cc3dae609d8e219b378848ce9
liuzehao <1437070389@qq.com>
2021-06-08 16:10:32 +0800

    统一返回封装类

--- src/main/java/com/example/erlysflexq/pojo/RqObject.java
+++ src/main/java/com/example/erlysflexq/pojo/RqObject.java
@@ -4,10 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RqObject {
-    Userinfo userInfo;
+    int STATUS;
     String token;
+
+    Userinfo userInfo;
+    List<Userinfo> userinfoList;
+
+    Arrangement arrangement;
+    List<Arrangement> arrangementList;
+
 }
