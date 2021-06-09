a5f1dcf3494c467ff5d3c755bc916018a4e50787
liuzehao <1437070389@qq.com>
2021-06-09 12:34:47 +0800

    使用jwt

--- src/main/java/com/example/erlysflexq/config/shiro/MyRealm.java
+++ src/main/java/com/example/erlysflexq/config/shiro/MyRealm.java
@@ -1,15 +1,16 @@
 package com.example.erlysflexq.config.shiro;
 
 import com.example.erlysflexq.pojo.Userinfo;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import com.example.erlysflexq.utils.JwtUtil;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.teasoft.bee.erlys.gets.GetMyWish;
 import org.teasoft.bee.osql.SuidRich;
@@ -21,10 +22,22 @@ import java.util.Set;
 @Component
 public class MyRealm extends AuthorizingRealm {
 
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @Value("${flexq.jwt.secret}")
+    private String secret;
+
 //    @Autowired
     SuidRich suidRich = BeeFactory.getHoneyFactory().getSuidRich();
     {
         super.setName("customRealm");
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof Token;
     }
 
     @Override
@@ -46,19 +59,23 @@ public class MyRealm extends AuthorizingRealm {
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         
-        String account = (String) authenticationToken.getPrincipal();
-        
-        String password = getPasswordByAccount(account);
+        String token = (String) authenticationToken.getPrincipal();
 
-        SimpleAuthenticationInfo authInfo = new SimpleAuthenticationInfo(account, password, "customRealm");
-        
+        Userinfo userinfo = suidRich.selectById(new Userinfo(), jwtUtil.getUserId(token));
+        jwtUtil.verifyToken(token);
+
+        if(userinfo == null){
+            throw new LockedAccountException("账号被锁定");
+        }
+        SimpleAuthenticationInfo authInfo = new SimpleAuthenticationInfo(userinfo, token, "customRealm");
+//        authInfo.setCredentialsSalt(ByteSource.Util.bytes(secret));
         return authInfo;
     }
 
-    private String getPasswordByAccount(String account) {
-        Userinfo userinfo = GetMyWish.selectByAccount(new Userinfo(), account);
-        if(userinfo == null)
-            return null;
-        return userinfo.getPassword();
-    }
+//    private String getPasswordByAccount(String account) {
+//        Userinfo userinfo = GetMyWish.selectByAccount(new Userinfo(), account);
+//        if(userinfo == null)
+//            return null;
+//        return userinfo.getPassword();
+//    }
 }
