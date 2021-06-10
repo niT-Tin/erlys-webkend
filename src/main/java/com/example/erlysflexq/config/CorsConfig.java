441108a15c7367c8e5dcafa7e31387ec75db8717
liuzehao <1437070389@qq.com>
2021-06-11 00:13:32 +0800

    跨域配置文件修改

--- src/main/java/com/example/erlysflexq/config/CorsConfig.java
+++ src/main/java/com/example/erlysflexq/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.setAllowCredentials(true);
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
