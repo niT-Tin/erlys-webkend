c3f11df6f645d2e2d8506f5ff13fbedf80647b0f
liuzehao <1437070389@qq.com>
2021-06-11 14:12:02 +0800

    修改局部

--- src/main/java/com/example/erlysflexq/utils/CompareSameInfo.java
+++ src/main/java/com/example/erlysflexq/utils/CompareSameInfo.java
@@ -11,22 +11,26 @@ import java.lang.reflect.Field;
 public class CompareSameInfo {
 
     public static Userinfo cp(Userinfo update) throws IllegalAccessException {
-        Userinfo userinfo = GetMyWish.selectByName(update, update.getName());
-        if(userinfo == null)
-            return null;
+//        Userinfo userinfo = GetMyWish.selectByName(new Userinfo(), update.getName());
+//        if(userinfo == null)
+//            return null;
+//
+//        Field[] fields = userinfo.getClass().getDeclaredFields();
+//        Field[] fields1 = update.getClass().getDeclaredFields();
+//
+//        for (int i = 0; i < fields.length; i++) {
+//            fields[i].setAccessible(true);
+//            fields1[i].setAccessible(true);
+//            if((!((fields[i].get(userinfo)) == null)) || (fields[i].get(userinfo) != fields1[i].get(update))){
+//                fields1[i].set(update, fields[i].get(userinfo));
+//            }else if ((!((fields1[i].get(update)) == null)) || (fields[i].get(userinfo) != fields1[i].get(update)))
+//                fields[i].set(userinfo, fields1[i].get(update));
+//        }
 
-        Field[] fields = userinfo.getClass().getDeclaredFields();
-        Field[] fields1 = update.getClass().getDeclaredFields();
+        //修改直接返回修改后的信息
+        System.out.println(update);
+        return update;
 
-        for (int i = 0; i < fields.length; i++) {
-            fields[i].setAccessible(true);
-            fields1[i].setAccessible(true);
-            if(!((fields[i].get(userinfo)) == null)){
-                fields1[i].set(update, fields[i].get(userinfo));
-            }
-        }
-
-        return userinfo;
     }
 
 }
