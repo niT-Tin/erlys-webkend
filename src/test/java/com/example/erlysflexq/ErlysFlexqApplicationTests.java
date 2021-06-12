970baed2b223fa7fd93ca5424b3bfb5eae32a55e
liuzehao <1437070389@qq.com>
2021-06-12 16:18:02 +0800

    测试类

--- src/test/java/com/example/erlysflexq/ErlysFlexqApplicationTests.java
+++ src/test/java/com/example/erlysflexq/ErlysFlexqApplicationTests.java
@@ -12,19 +12,6 @@ class ErlysFlexqApplicationTests {
 
     @Test
     void contextLoads() {
-        Userinfo userinfo = GetMyWish.selectByName(new Userinfo(), "chao");
-        System.out.println(userinfo);
-        Userinfo u = new Userinfo();
-        u.setSec(99L);
-        u.setName(userinfo.getName());
-//        System.out.println(u);
-        System.out.println("--------------------------------------------------");
-        try {
-            Userinfo cp = CompareSameInfo.cp(u);
-            System.out.println(cp);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
     }
 
 }
