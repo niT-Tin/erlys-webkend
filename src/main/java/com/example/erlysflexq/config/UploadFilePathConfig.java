8798ad1a7a68325812ef8828c9ac7bb02d11ee3b
liuzehao <1437070389@qq.com>
2021-06-13 20:34:05 +0800

    上传文件路径配置

---
+++ src/main/java/com/example/erlysflexq/config/UploadFilePathConfig.java
@@ -0,0 +1,23 @@
+package com.example.erlysflexq.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+//@Component
+public class UploadFilePathConfig implements WebMvcConfigurer {
+
+    @Value("${file.staticAccessPath}")
+    private String staticAccessPath;
+    @Value("${file.uploadFolder}")
+    private String uploadFolder;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(staticAccessPath).addResourceLocations("file:///" + uploadFolder);
+    }
+}
